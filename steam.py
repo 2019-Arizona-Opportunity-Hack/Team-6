d000d55a936a225920e5910c2d32dac2a9c2f34f
rushabh0701 <54603567+rushabh0701@users.noreply.github.com>
2019-10-20 09:13:16 -0700

    Updated file to create table

--- steam.py
+++ steam.py
@@ -1,12 +1,29 @@
 import requests
 import lxml.html
 import pandas as pd
+import plotly.graph_objects as go
 
 html = requests.get('https://store.steampowered.com/search/?filter=topsellers')
 doc = lxml.html.fromstring(html.content)
+
+# new_releases = doc.xpath('//div[@id="tab_topsellers_content"]')[0]
 
 titles = doc.xpath('.//div[@id="search_result_container"]')[0]
 
 span = titles.xpath('.//span[@class="title"]/text()')
 
-tbl = pd.DataFrame(span, columns=['Top Selling Games'])
+tbl = pd.DataFrame(span, columns=['Games'])
+arr = tbl.values
+
+fig = go.Figure(data=[go.Table(
+    header=dict(values=['Top Selling Games'],
+                line_color='darkslategray',
+                fill_color='lightskyblue',
+                align='left'),
+    cells=dict(values=[arr], # 2nd column
+               line_color='darkslategray',
+               fill_color='lightcyan',
+               align='left'))
+])
+
+fig.show()
