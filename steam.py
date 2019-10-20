442c5b8a5ce823a66f5760c011c127b99dde1d1f
rushabh0701 <54603567+rushabh0701@users.noreply.github.com>
2019-10-19 22:27:04 -0700

    Add files via upload

--- steam.py
+++ steam.py
@@ -1,64 +1,14 @@
+import requests
+import lxml.html
 import pandas as pd
-import chart_studio.plotly as py
-from pytrends.request import TrendReq
-import plotly.express as px
-import chart_studio.tools as tls
 
+html = requests.get('https://store.steampowered.com/search/?filter=topsellers')
+doc = lxml.html.fromstring(html.content)
 
-pytrends = TrendReq(hl='en-US', tz=360)
-def related(list1, list2): # find if the the 2 lists are related
-    for i in list1:
-        for j in list2:
-            if i in j:
-                return True
-    return False
-def showInfo():
-    info = pd.read_csv("Fan_Fusion_Panels.csv", encoding ="ANSI")
-    print(info)
-#showInfo()
-def toLower(text): # make string to lowercase
-    newText = text.lower()
-    return newText
-def removeSpecialChar(text): # get rid of special characters
-    k = text.translate ({ord(c): " " for c in "!@#$%^&*()[]{};:,./<>?\|`~=_+"})
-    return k
-def findTrends(keywords):
-    trending =[]
-    df = pytrends.trending_searches(pn='united_states')
-    for i in df[0]:
-        #print(i)
-        kw_list = [i]
-        pytrends.build_payload(kw_list, cat=0, timeframe='today 5-y', geo='', gprop='')
-        top = pytrends.related_topics() #find related topic
-        k = removeSpecialChar(i)
-        topics = top[k]['top']['topic_type']
-        lowerTop = topics.apply(toLower)
-        topics = lowerTop.apply(removeSpecialChar)
-        list_topic = list(topics)
-        #print(list_topic)
-        if related(keywords, list_topic):
-            trending.append(str(i))
-    return trending
-def embedPlotly(html):
-    tls.get_embed(html)
-def plotInterest(list):
-    kw_list = [list]
-    pytrends.build_payload(kw_list, cat=0, timeframe='today 5-y', geo='', gprop='')
-    interest = pytrends.interest_over_time()
-    df = interest[list]
-    x_axis = df.index.to_frame()
-    #print(x_axis)
-    fig = px.line(df,x = x_axis['date'],  y=df)
-    py.iplot(fig, filename = 'starwars')
-    first_plot_url = py.plot(fig, filename='starwars', auto_open=False,)
+titles = doc.xpath('.//div[@id="search_result_container"]')[0]
 
-    embedPlotly(first_plot_url)
-    result = first_plot_url+".embed"
-    return result
-    """print(df)
-    df1 = pytrends.suggestions("game")
-    print(df1)"""
-#trending = findTrends(["film"])
-print(plotInterest("Star wars"))
+span = titles.xpath('.//span[@class="title"]/text()')
 
+tbl = pd.DataFrame(span, columns=['Top Selling Games'])
 
+print(tbl)
